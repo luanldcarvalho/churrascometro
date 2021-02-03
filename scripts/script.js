fb63a953996e92df9db909dd4523da391a334df9
luanldcarvalho <luanldcarvalho@gmail.com>
2021-02-03 13:02:40 -0300

    churrascometro

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+let inputAdultos = document.getElementById('adultos')
+let inputCriancas = document.getElementById('criancas')
+let inputDuracao = document.getElementById('duracao')
+
+let resultado = document.getElementById('resultado')
+
+function calcular(){
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qdtTotalCarne = (carnePP(duracao) * adultos) + (carnePP(duracao)/2 * criancas)
+    let qdtTotalCerveja = (cervejaPP(duracao) * adultos)
+    let qdtTotalBebidas = (bebidasPP(duracao) * adultos) + (bebidasPP(duracao)/2 * criancas)
+
+    resultado.innerHTML = `<h2>Resultado:</h2>`;
+    resultado.innerHTML = `<h4>Para ${adultos} adultos e ${criancas} crianças durante ${duracao} horas:</h4>`;
+    resultado.innerHTML += `<p>${qdtTotalCarne/1000} Kg de Carne.</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja/1000)} L de Cerveja.</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas/2000)} L de Bebidas.</p>`
+
+}
+
+function carnePP(duracao){
+    if(duracao >= 6){
+        return 700;
+    }else{
+        return 400;
+    }
+}
+
+function cervejaPP(duracao){
+    if(duracao >= 6){
+        return 2000;
+    }else{
+        return 1200;
+    }
+}
+
+function bebidasPP(duracao){
+    if(duracao >= 6){
+        return 1500;
+    }else{
+        return 1000;
+    }
+}
